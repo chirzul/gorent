00578b64e57824257017d3f75f90160fbd258fb8
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-09-16 16:16:52 +0700

    Add interfaces for histories, users, vehicles

--- src/interfaces/histories.go
+++ src/interfaces/histories.go
@@ -1,1 +1,21 @@
 package interfaces
+
+import (
+	"net/http"
+
+	"github.com/chirzul/gorent/src/databases/orm/models"
+)
+
+type HistoryRepo interface {
+	FindAllHistories() (*models.Histories, error)
+	SaveHistory(data *models.History) (*models.History, error)
+	ChangeHistory(r *http.Request, data *models.History) (*models.History, error)
+	RemoveHistory(r *http.Request, data *models.History) (*models.History, error)
+}
+
+type HistoryService interface {
+	GetAllHistories() (*models.Histories, error)
+	AddHistory(data *models.History) (*models.History, error)
+	UpdateHistory(r *http.Request, data *models.History) (*models.History, error)
+	DeleteHistory(r *http.Request, data *models.History) (*models.History, error)
+}
--- src/interfaces/users.go
+++ src/interfaces/users.go
@@ -1,1 +1,21 @@
 package interfaces
+
+import (
+	"net/http"
+
+	"github.com/chirzul/gorent/src/databases/orm/models"
+)
+
+type UserRepo interface {
+	FindAllUsers() (*models.Users, error)
+	SaveUser(data *models.User) (*models.User, error)
+	ChangeUser(r *http.Request, data *models.User) (*models.User, error)
+	RemoveUser(r *http.Request, data *models.User) (*models.User, error)
+}
+
+type UserService interface {
+	GetAllUsers() (*models.Users, error)
+	AddUser(data *models.User) (*models.User, error)
+	UpdateUser(r *http.Request, data *models.User) (*models.User, error)
+	DeleteUser(r *http.Request, data *models.User) (*models.User, error)
+}
