e2ab2b4af225315ba29b2a016c9c9f8b549263c7
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-09-16 20:59:31 +0700

    Add relation to user and vehicle tables

--- src/databases/orm/models/histories.go
+++ src/databases/orm/models/histories.go
@@ -5,11 +5,13 @@ import "time"
 type History struct {
 	HistoryID     uint      `gorm:"primaryKey" json:"history_id"`
 	UserID        string    `json:"user_id"`
+	User          User      `json:"user_data"`
 	VehicleID     uint      `json:"vehicle_id"`
+	Vehicle       Vehicle   `json:"vehicle_data"`
 	StartRent     string    `json:"start_rent"`
-	EndRent       uint      `json:"end_rent"`
-	Prepayment    string    `json:"prepayment"`
-	PaymentStatus uint      `json:"payment_status"`
+	EndRent       string    `json:"end_rent"`
+	Prepayment    uint      `json:"prepayment"`
+	PaymentStatus string    `json:"payment_status"`
 	ReturnStatus  string    `json:"return_status"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
