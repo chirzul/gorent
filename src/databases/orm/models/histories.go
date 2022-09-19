3160e0f256cf65d976afeee785f63ca320155946
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-09-19 13:45:08 +0700

    add omitempty option for model struct

--- src/databases/orm/models/histories.go
+++ src/databases/orm/models/histories.go
@@ -3,18 +3,18 @@ package models
 import "time"
 
 type History struct {
-	HistoryID     uint      `gorm:"primaryKey" json:"history_id"`
-	UserID        string    `json:"user_id"`
+	HistoryID     uint      `gorm:"primaryKey" json:"history_id,omitempty"`
+	UserID        string    `json:"user_id,omitempty"`
 	User          User      `json:"user_data"`
-	VehicleID     uint      `json:"vehicle_id"`
+	VehicleID     uint      `json:"vehicle_id,omitempty"`
 	Vehicle       Vehicle   `json:"vehicle_data"`
-	StartRent     string    `json:"start_rent"`
-	EndRent       string    `json:"end_rent"`
-	Prepayment    uint      `json:"prepayment"`
-	PaymentStatus string    `json:"payment_status"`
-	ReturnStatus  string    `json:"return_status"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"updated_at"`
+	StartRent     string    `json:"start_rent,omitempty"`
+	EndRent       string    `json:"end_rent,omitempty"`
+	Prepayment    uint      `json:"prepayment,omitempty"`
+	PaymentStatus string    `json:"payment_status,omitempty"`
+	ReturnStatus  string    `json:"return_status,omitempty"`
+	CreatedAt     time.Time `json:"created_at,omitempty"`
+	UpdatedAt     time.Time `json:"updated_at,omitempty"`
 }
 
 type Histories []History
--- src/databases/orm/models/vehicles.go
+++ src/databases/orm/models/vehicles.go
@@ -3,17 +3,18 @@ package models
 import "time"
 
 type Vehicle struct {
-	VehicleID   uint      `gorm:"primaryKey" json:"vehicle_id"`
-	Name        string    `json:"name"`
-	Location    string    `json:"location"`
-	Description string    `json:"description"`
-	Price       uint      `json:"price"`
-	Status      string    `json:"status"`
-	Stock       uint      `json:"stock"`
-	Category    string    `json:"category"`
-	Image       string    `json:"image"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	VehicleID   uint      `gorm:"primaryKey" json:"vehicle_id,omitempty"`
+	Name        string    `json:"name,omitempty"`
+	Location    string    `json:"location,omitempty"`
+	Description string    `json:"description,omitempty"`
+	Price       uint      `json:"price,omitempty"`
+	Status      string    `json:"status,omitempty"`
+	Stock       uint      `json:"stock,omitempty"`
+	Category    string    `json:"category,omitempty"`
+	Image       string    `json:"image,omitempty"`
+	CreatedAt   time.Time `json:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty"`
+	TotalRented int       `json:"total_rented"`
 }
 
 type Vehicles []Vehicle
