92b01318454601c115364b109e6485c5cfdd241c
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-09-19 13:47:47 +0700

    add routers

--- src/routers/routers.go
+++ src/routers/routers.go
@@ -1,1 +1,26 @@
 package routers
+
+import (
+	"errors"
+
+	"github.com/chirzul/gorent/src/databases/orm"
+	"github.com/chirzul/gorent/src/modules/v1/histories"
+	"github.com/chirzul/gorent/src/modules/v1/users"
+	"github.com/chirzul/gorent/src/modules/v1/vehicles"
+	"github.com/gorilla/mux"
+)
+
+func New() (*mux.Router, error) {
+	mainRoute := mux.NewRouter()
+
+	db, err := orm.New()
+	if err != nil {
+		return nil, errors.New("gagal init database")
+	}
+
+	users.New(mainRoute, db)
+	vehicles.New(mainRoute, db)
+	histories.New(mainRoute, db)
+
+	return mainRoute, nil
+}
