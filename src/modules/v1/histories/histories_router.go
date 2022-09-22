f6bc20957415ddb7056329aa42b6793df053ae4a
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-09-22 18:33:26 +0700

    change router to v1

--- src/modules/v1/histories/histories_router.go
+++ src/modules/v1/histories/histories_router.go
@@ -6,15 +6,15 @@ import (
 )
 
 func New(rt *mux.Router, db *gorm.DB) {
-	route := rt.PathPrefix("/histories").Subrouter()
+	route := rt.PathPrefix("/api/v1/histories").Subrouter()
 
 	repo := NewRepo(db)
 	svc := NewService(repo)
 	ctrl := NewCtrl(svc)
 
-	route.HandleFunc("/", ctrl.GetAllHistories).Methods("GET")
+	route.HandleFunc("", ctrl.GetAllHistories).Methods("GET")
+	route.HandleFunc("", ctrl.AddHistory).Methods("POST")
 	route.HandleFunc("/search/", ctrl.SearchHistory).Methods("GET")
-	route.HandleFunc("/", ctrl.AddHistory).Methods("POST")
 	route.HandleFunc("/{history_id}", ctrl.UpdateHistory).Methods("PUT")
 	route.HandleFunc("/{history_id}", ctrl.DeleteHistory).Methods("DELETE")
 }
