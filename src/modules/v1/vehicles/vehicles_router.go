73496ea9ccc03aa95e106360e9bc68b588ac7b8d
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-09-22 18:33:40 +0700

    change router to v1

--- src/modules/v1/vehicles/vehicles_router.go
+++ src/modules/v1/vehicles/vehicles_router.go
@@ -6,16 +6,16 @@ import (
 )
 
 func New(rt *mux.Router, db *gorm.DB) {
-	route := rt.PathPrefix("/vehicles").Subrouter()
+	route := rt.PathPrefix("/api/v1/vehicles").Subrouter()
 
 	repo := NewRepo(db)
 	svc := NewService(repo)
 	ctrl := NewCtrl(svc)
 
-	route.HandleFunc("/", ctrl.GetAllVehicles).Methods("GET")
+	route.HandleFunc("", ctrl.GetAllVehicles).Methods("GET")
+	route.HandleFunc("", ctrl.AddVehicle).Methods("POST")
 	route.HandleFunc("/popular/", ctrl.GetPopularVehicles).Methods("GET")
 	route.HandleFunc("/search/", ctrl.SearchVehicles).Methods("GET")
-	route.HandleFunc("/", ctrl.AddVehicle).Methods("POST")
 	route.HandleFunc("/{vehicle_id}", ctrl.UpdateVehicle).Methods("PUT")
 	route.HandleFunc("/{vehicle_id}", ctrl.DeleteVehicle).Methods("DELETE")
 }
