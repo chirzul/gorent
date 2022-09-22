5bfa51311796f3ca7e1627921f22478dc6c97e86
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-09-22 18:34:26 +0700

    change router to v1

--- src/modules/v1/users/users_router.go
+++ src/modules/v1/users/users_router.go
@@ -1,19 +1,21 @@
 package users
 
 import (
+	"github.com/chirzul/gorent/src/middlewares"
 	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
 func New(rt *mux.Router, db *gorm.DB) {
-	route := rt.PathPrefix("/users").Subrouter()
+	route := rt.PathPrefix("/api/v1/users").Subrouter()
 
 	repo := NewRepo(db)
 	svc := NewService(repo)
 	ctrl := NewCtrl(svc)
 
-	route.HandleFunc("/", ctrl.GetAllUsers).Methods("GET")
-	route.HandleFunc("/", ctrl.AddUser).Methods("POST")
+	route.HandleFunc("", middlewares.CheckAuth(ctrl.GetAllUsers)).Methods("GET")
+	route.HandleFunc("/{username}", ctrl.FindByUsername).Methods("GET")
+	route.HandleFunc("", ctrl.AddUser).Methods("POST")
 	route.HandleFunc("/{username}", ctrl.UpdateUser).Methods("PUT")
 	route.HandleFunc("/{username}", ctrl.DeleteUser).Methods("DELETE")
 }
