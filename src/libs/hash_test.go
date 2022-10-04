0e01b65ddf0ee443e089b676f9bd693cf8be72f0
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-10-04 10:12:31 +0700

    add assertion

--- src/libs/hash_test.go
+++ src/libs/hash_test.go
@@ -17,6 +17,12 @@ func TestHashPassword(t *testing.T) {
 func TestCheckPassword(t *testing.T) {
 	inputPass := "user"
 	dbPass := "$2a$10$3EIKZ6FemRn2cblEioUUKuFDkEKDxd3RqkIWxXev7PEpLdoLaDwg2"
-	assert.True(t, CheckPassword(dbPass, inputPass), "Result must be true")
-	assert.False(t, CheckPassword(dbPass, "falsepassword"), "Result must be false")
+
+	t.Run("Check true password", func(t *testing.T) {
+		assert.True(t, CheckPassword(dbPass, inputPass), "Result must be true")
+	})
+
+	t.Run("Check wrong password", func(t *testing.T) {
+		assert.False(t, CheckPassword(dbPass, "falsepassword"), "Result must be false")
+	})
 }
