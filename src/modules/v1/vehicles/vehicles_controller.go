a6c307e9f8fe7a505675b354147240dc783aa119
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-10-09 21:25:34 +0700

    Add CORS

--- src/modules/v1/vehicles/vehicles_controller.go
+++ src/modules/v1/vehicles/vehicles_controller.go
@@ -22,14 +22,6 @@ func NewCtrl(s interfaces.VehicleService) *vehicle_ctrl {
 }
 
 func (c *vehicle_ctrl) GetAllVehicles(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
-
-	if r.Method == "OPTIONS" {
-		w.Write([]byte("allowed"))
-		return
-	}
 	c.svc.GetAllVehicles().Send(w)
 }
 
